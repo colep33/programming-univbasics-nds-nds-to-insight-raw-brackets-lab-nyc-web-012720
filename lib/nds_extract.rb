0783218783a14921eb9cd2431adb9744d1bef50e
Cole Arthur Parham <coleparham@gmail.com>
2020-01-06 20:55:01 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -16,11 +16,7 @@ def directors_totals(nds)
     end
     row_index += 1
   end
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
+
   result = {
   }
   #
